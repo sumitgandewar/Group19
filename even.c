2fb200ae185f272e669d12a911cea6f8f142a7ad
kajaltoraskar <106860429+kajaltoraskar@users.noreply.github.com>
2022-06-04 23:17:12 +0530

    this change is done by kajal

---
+++ even.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+   int n;
+   printf("enter a number=");
+   scanf("%d",&n);
+   if(n%2==0)
+   printf("enter number is even");
+   else
+   printf("enter number is odd");
+   return 0;
+}
