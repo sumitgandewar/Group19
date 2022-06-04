de8b699679154b812cf91fefd7529de01a639c68
dvwaykar <106860459+dvwaykar@users.noreply.github.com>
2022-06-04 23:15:55 +0530

    This commit is done by Dipak
    alpha and beta files added

---
+++ Alpha.java
@@ -0,0 +1,5 @@
+package study.scopes;
+
+public class Alpha {
+
+}
---
+++ Beta.java
@@ -0,0 +1,12 @@
+package study.scopes;
+
+public class Beta {
+	
+	public static void test1()
+	{
+		Alpha A1 = new Alpha();
+		//A1.data = 300;
+	}
+
+
+}
