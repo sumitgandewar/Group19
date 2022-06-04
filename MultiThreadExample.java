4fa245d96a9bc1abb61dd8842aea591c32ae2aec
PRAAAAA123 <101059074+PRAAAAA123@users.noreply.github.com>
2022-06-04 23:14:57 +0530

    Thread example
    core java

---
+++ MultiThreadExample.java
@@ -0,0 +1,39 @@
+package study.threads;
+
+public class MultiThreadExample {
+
+	public static void main(String[] args) {
+		ChatThread th1 = new ChatThread();
+		VideoThread th2 = new VideoThread();
+		
+		th1.start(); //this method will create OS level thread and add it to ready queue
+		th2.start(); //when the schedular will select the thread at that time run() execute
+
+	}//end of main
+
+}//end of MultiThreadExample
+
+class ChatThread extends Thread
+{
+	@Override
+	public void run() {
+	
+		while(true)
+			System.out.println("zoom chat is running");
+	
+	}
+}
+
+class VideoThread extends Thread
+{
+	@Override
+	public void run() {
+	
+		while(true)
+			System.out.println("zoom video is running");
+	
+	}
+}
+
+
+
