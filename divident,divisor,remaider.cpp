d06d1af216a80c490d473435e3cb3ab7d5307548
AkashYadav777 <106860449+AkashYadav777@users.noreply.github.com>
2022-06-04 23:17:01 +0530

    created by Akash

---
+++ divident,divisor,remaider.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+	int dividend,divisor,quotient,remainder;
+	printf("Enter dividend : ");
+	scanf("%d",&dividend);
+	printf("Enter divisor : ");
+	scanf("%d",&divisor);
+	quotient = dividend/divisor;
+	remainder = dividend % divisor;
+	printf("quotient = %d/n",quotient);
+	printf("remainder = %d/:",remainder);
+	return 0;
+	
+	
+	
+}
