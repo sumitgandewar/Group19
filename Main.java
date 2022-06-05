1a2f1927d0149ba4619f3f90778a9b79fd86d054
unknown <prathameshdutondkar97@gmail.com>
2022-06-05 20:32:21 +0530

    Main.java file added

---
+++ Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int [] arr = new int[]{50, 20, 100, 5, 76, 75, 1};
+		
+		Sort.print(arr);
+		Sort.selection(arr);
+		Sort.print(arr);
+		
+	}
+}
